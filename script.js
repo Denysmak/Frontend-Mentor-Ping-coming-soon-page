3e1fb54f54d1215a6b22b080a30c6e558c43a114
Denysmak <denysmakene13@gmail.com>
2022-05-07 18:44:55 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+function facebook(e){
+e.src = "./images/facebook2.svg"
+}
+function facebookOut(e){
+e.src = "./images/facebook.svg"
+}
+function twitter(e){
+e.src = "./images/twitter2.svg"
+}
+function twitterOut(e){
+e.src = "./images/twitter.svg"
+}
+function instagram(e) {
+    e.src = "./images/instagram2.svg"
+}
+function instagramOut(e) {
+    e.src = "./images/instagram.svg"
+}
